f718914d6d371c2f4af8c568f92d48f5b6fa4471
edcoreweb <edcoreweb@gmail.com>
2015-11-25 17:56:36 +0200

    Domio textures

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,4 @@ import './../../bower_components/jquery.shapeshift/core/jquery.shapeshift.js'
 
 $(".board-game").shapeshift({
     minColumns: 100,
-    cutoffStart: 3,
-    cutoffEnd: 3,
-    dragWhitelist: ".active"
 });
