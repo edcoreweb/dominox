ba8cd8e10e70f55c6d167f7aa048208d08adc0b0
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-19 15:06:39 +0200

    Fix elixir source maps.
    npm install / update

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ process.env.DISABLE_NOTIFIER = true;
 var elixir = require('laravel-elixir');
 
 elixir.config.assetsPath = '';
+elixir.config.production = false;
+elixir.config.sourcemaps = true;
+elixir.config.publicPath = 'dist';
+elixir.config.js.browserify.options.debug = true;
 
 elixir(function(mix) {
     // Complie less.
-    mix.less('app.less', 'dist/app.css');
+    mix.less('app.less');
 
     // Compile JavaScript.
-    mix.browserify('app.js', 'dist/app.js', null, {debug: true});
+    mix.browserify('app.js');
 
     // Copy fonts.
     mix.copy('node_modules/bootstrap/dist/fonts', 'dist/fonts');
