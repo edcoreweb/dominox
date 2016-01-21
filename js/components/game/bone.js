cb1c463b4bfa2b3f3c9e5f96cf852b85226df2e0
edcoreweb <edcoreweb@gmail.com>
2016-01-21 23:22:21 +0200

    JS logic refactoring.

--- js/components/game/bone.js
+++ js/components/game/bone.js
@@ -2,76 +2,144 @@ Vue.component('bone', {
     template: require('./../../templates/game/bone.html'),
 
     props: {
-        model: Object
+        piece: null
     },
 
     data() {
         return {
             parent: null,
-            classes: {}
+            positionClasses: {},
+            placeholderClasses: {},
+            placeholders: [],
+            hasPlaceholders: false
         };
     },
 
     created() {
-        this.parent = this.$parent.constructor.name == 'Bone' ? this.$parent : null;
-
-        this.classes = this.getClassObject();
-    },
-
-    computed: {
-        hasChildren: function () {
-            return this.model.children &&
-                this.model.children.length;
-        }
+        this.parent = this.getParent();
+        this.positionClasses = this.getPositionClassObject();
     },
 
     methods: {
         select() {
-            this.model.children.push(
-                {
-                    name: '2',
-                    corner: null,
-                    vertical: false,
-                    direction: 'left',
-                    children: [
 
-                    ]
-                });
+            console.log(this.hasPlaceholders);
+
+            if (!this.hasPlaceholders) {
+                this.setPlaceholderClassObject();
+            } else {
+                this.clearPlaceholderClassObject();
+            }
         },
 
-        getClassObject() {
+        hasChildren() {
+            return this.piece.hasChildren();
+        },
+
+        getChildren() {
+            return this.piece.getChildren();
+        },
+
+        generatePlaceholders() {
+
+
+
+        },
+
+        /**
+         * Get the position of node relative to parent.
+         * @return {Object} style object
+         */
+        getPositionClassObject() {
             let style = {};
 
-            // if (this.parent) {
-
-            //     this.switch = (this.parent.model.vertical != this.model.vertical);
-            //     this.counter = (this.parent.model.vertical) ;
-
-            //     style.switch = this.switch;
-            //     style.counter = this.counter;
-
-            // }
-
-            // if (this.switch) {
-            //     style.vertical = this.model.vertical;
-            //     style.horizontal = !this.model.vertical;
-            // }
-
-            style.vertical = this.model.vertical;
-            style.horizontal = !this.model.vertical;
+            style.vertical = this.piece.getVertical();
+            style.horizontal = !this.piece.getVertical();
 
             if (this.parent) {
-                style.counter = (this.parent.classes.vertical && !this.model.vertical);
-                style.rotate = (!this.parent.classes.vertical && this.model.vertical);
+                style.counter = (this.parent.positionClasses.vertical && !this.piece.getVertical());
+                style.rotate = (!this.parent.positionClasses.vertical && this.piece.getVertical());
+            } else if (this.piece.getVertical()) {
+                style.rotate = true;
             }
 
-            style[this.model.direction] = true;
+            style[this.piece.getDirection()] = true;
 
-            if (this.model.corner) {
-                style['corner-' + this.model.corner] = true;
+            if (this.piece.getCorner()) {
+                style['corner-' + this.piece.getCorner()] = true;
             }
 
             return style;
+        },
+
+        /**
+         * Get the position of node relative to parent.
+         * @return {Object} style object
+         */
+        getPlaceholderClassObject() {
+            let style = {};
+
+            style.placeholder = true;
+
+            if (this.positionClasses['corner-down']) {
+                style['placeholder-down'] = true;
+            } else if (this.positionClasses['corner-up']) {
+                style['placeholder-up'] = true;
+            } else if (this.positionClasses['right']) {
+                style['placeholder-down'] = true;
+            } else if (this.positionClasses['left']) {
+                style['placeholder-up'] = true;
+            } else {
+                style['placeholder-full'] = true;
+            }
+
+            return style;
+        },
+
+        /**
+         * Sets the placeholder styling.
+         */
+        setPlaceholderClassObject() {
+            this.placeholderClasses = this.getPlaceholderClassObject();
+            this.hasPlaceholders = true;
+        },
+
+        /**
+         * Clears the placeholder styling.
+         */
+        clearPlaceholderClassObject() {
+            this.placeholderClasses = {};
+            this.hasPlaceholders = false;
+        },
+
+        /**
+         * Check if parent node is Bone.
+         * @return {Boolean}
+         */
+        isParentBone() {
+            return this.$parent.constructor.name == 'Bone';
+        },
+
+        /**
+         * Get the parent node.
+         * @return {Bone | null}
+         */
+        getParent() {
+            return this.isParentBone() ? this.$parent : null;
+        },
+
+        /**
+         * Get the root of the table tree.
+         * @return {Bone}
+         */
+        getRoot() {
+            let rootNode = this.getParent();
+
+            while(rootNode && rootNode.getParent()) {
+                rootNode = rootNode.getParent();
+            }
+
+            return rootNode ? rootNode : this;
         }
     }
 });
--- js/components/game/main.js
+++ js/components/game/main.js
@@ -1,212 +1,39 @@
 // import Auth from './../../Auth';
-// import Piece from './../../Piece';
+import Piece from './../../Piece';
 import './bone';
 import 'jquery.panzoom';
 
-var data = {
-    name: '1',
-    corner: null,
-    vertical: false,
-    direction: 'center',
-    children: [
-        {
-            name: '2',
-            corner: null,
-            vertical: false,
-            direction: 'left',
-            children: [
+let rootNode = new Piece('1', false, 'root', null);
 
-            ]
-        },
+rootNode.addChildren([
+    new Piece('2', false, 'left', null),
+    new Piece('3', true, 'left', 'up'),
+    new Piece('4', true, 'left', 'down'),
+    new Piece('5', true, 'right', 'up'),
+    new Piece('6', true, 'right', 'down'),
+    new Piece('7', false, 'right', null)
+]);
 
-        {
-            name: '3',
-            corner: null,
-            vertical: true,
-            direction: 'up',
-            children: [
-
-                {
-                    name: '20',
-                    corner: 'up',
-                    vertical: false,
-                    direction: 'up',
-                    children: [
-
-                    ]
-                }
-
-            ]
-        },
-
-        {
-            name: '4',
-            corner: null,
-            vertical: true,
-            direction: 'down',
-            children: [
-
-                {
-                    name: '21',
-                    corner: 'down',
-                    vertical: false,
-                    direction: 'down',
-                    children: [
-
-                    ]
-                }
-
-            ]
-        },
-
-        {
-            name: '5',
-            corner: null,
-            vertical: false,
-            direction: 'right',
-            children: [
-
-                {
-                    name: '6',
-                    corner: 'down',
-                    vertical: true,
-                    direction: 'right',
-                    children: [
-
-                        {
-                            name: '8',
-                            corner: null,
-                            vertical: true,
-                            direction: 'up',
-                            children: [
-
-                                {
-                                    name: '10',
-                                    corner: null,
-                                    vertical: true,
-                                    direction: 'up',
-                                    children: [
-
-                                    ]
-                                }
-
-                            ]
-                        },
-
-                        {
-                            name: '9',
-                            corner: null,
-                            vertical: true,
-                            direction: 'down',
-                            children: [
-
-                            ]
-                        },
-
-                        {
-                            name: '7',
-                            corner: 'down',
-                            vertical: false,
-                            direction: 'right',
-                            children: [
-
-                                {
-                                    name: '11',
-                                    corner: 'down',
-                                    vertical: true,
-                                    direction: 'up',
-                                    children: [
-
-                                        {
-                                            name: '12',
-                                            corner: null,
-                                            vertical: true,
-                                            direction: 'up',
-                                            children: [
-
-                                                {
-                                                    name: '13',
-                                                    corner: 'up',
-                                                    vertical: false,
-                                                    direction: 'right',
-                                                    children: [
-
-                                                    ]
-                                                }
-
-                                            ]
-                                        }
-
-                                    ]
-                                }
-
-                            ]
-                        }
-
-                    ]
-                }
-
-                // {
-                //     name: '12',
-                //     corner: null,
-                //     vertical: false,
-                //     direction: 'down',
-                //     children: [
-
-                //     ]
-                // },
-
-                // {
-                //     name: '5',
-                //     corner: null,
-                //     vertical: false,
-                //     direction: 'right',
-                //     children: [
-
-                //         {
-                //             name: '6',
-                //             corner: null,
-                //             vertical: true,
-                //             direction: 'right',
-                //             children: [
-
-                //                 {
-                //                     name: '13',
-                //                     corner: null,
-                //                     vertical: false,
-                //                     direction: 'up',
-                //                     children: [
-
-                //                     ]
-                //                 },
-
-                //                 {
-                //                     name: '14',
-                //                     corner: null,
-                //                     vertical: false,
-                //                     direction: 'down',
-                //                     children: [
-
-                //                     ]
-                //                 },
-
-                //                 {
-                //                     name: '7',
-                //                     corner: null,
-                //                     vertical: false,
-                //                     direction: 'right',
-                //                     children: [
-
-                //                     ]
-                //                 }
-                //             ]
-                //         }
-                //     ]
-                // }
-            ]
-        }
-    ]
-};
+var playerCards = [
+    {
+        name: '1',
+        corner: null,
+        vertical: true,
+        direction: 'root'
+    },
+    {
+        name: '1',
+        corner: null,
+        vertical: true,
+        direction: 'right'
+    },
+    {
+        name: '1',
+        corner: null,
+        vertical: true,
+        direction: 'right'
+    }
+];
 
 module.exports = {
     template: require('./../../templates/game/main.html'),
@@ -214,22 +41,13 @@ module.exports = {
     data() {
         return {
             selected: null,
-            boardData: data
+            root: rootNode,
+            bones: playerCards
         };
     },
 
     ready() {
 
-        var $panzoom = $('.outer-div').panzoom();
-        $panzoom.parent().on('mousewheel.focal', function( e ) {
-            var delta = e.delta || e.originalEvent.wheelDelta;
-            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-            $panzoom.panzoom('zoom', zoomOut, {
-                increment: 0.1,
-                animate: false,
-                focal: e
-            });
-        });
     },
 
     methods: {
