fd09252000e5f3eae98eb575a054b3b1636e80f1
edcoreweb <edcoreweb@gmail.com>
2016-01-24 17:02:36 +0200

    Updated Piece class.

---
+++ js/Const.js
@@ -0,0 +1,6 @@
+let Const = {
+    PIECE_WIDTH : 2,
+    PIECE_HEIGHT : 1
+}
+
+module.exports = Const;
--- js/Piece.js
+++ js/Piece.js
@@ -1,3 +1,4 @@
+import Const from './Const';
 
 class Piece {
     /**
@@ -13,7 +14,13 @@ class Piece {
         this.first = parseInt(this.name.charAt(0));
         this.second = parseInt(this.name.charAt(1));
 
-        return this;
+        if (this.vertical) {
+            this.width = Const.PIECE_HEIGHT;
+            this.height = Const.PIECE_WIDTH;
+        } else {
+            this.width = Const.PIECE_WIDTH;
+            this.height = Const.PIECE_HEIGHT;
+        }
     }
 
     addChild(child) {
@@ -76,6 +83,100 @@ class Piece {
 
     getChildren() {
         return this.children;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    getWidth() {
+        return this.width;
+    }
+
+    setCoords(coords) {
+        this.coords = coords;
+    }
+
+    getCoords() {
+        return this.coords;
+    }
+
+    /**
+     * Calculate grid position relative to parent
+     * @param  {Piece} piece
+     * @param  {Piece} parent
+     */
+    calculateCoords(piece, parent) {
+        let coords = {};
+
+        if (piece.direction == 'root' || !parent) {
+            coords.x = 0;
+            coords.y = 0;
+
+            piece.setCoords(coords);
+            return;
+        }
+
+        let pCoords = parent.getCoords();
+
+        switch(piece.direction) {
+            case 'up':
+                if (piece.corner == 'up') {
+                    coords.x = pCoords.x;
+                    coords.y = pCoords.y + parent.getHeight();
+                } else if (piece.corner == 'down') {
+                    coords.x = pCoords.x + parent.getHeight();
+                    coords.y = pCoords.y + parent.getHeight();
+                } else {
+                    coords.x = pCoords.x;
+                    coords.y = pCoords.y + parent.getHeight();
+                }
+                break;
+
+            case 'down':
+                if (piece.corner == 'up') {
+                    coords.x = pCoords.x;
+                    coords.y = pCoords.y - piece.height;
+                } else if (piece.corner == 'down') {
+                    coords.x = pCoords.x + parent.getHeight();
+                    coords.y = pCoords.y - piece.height;
+                } else {
+                    coords.x = pCoords.x;
+                    coords.y = pCoords.y - piece.height;
+                }
+                break;
+
+            case 'left':
+                if (piece.corner == 'up') {
+                    coords.x = pCoords.x - piece.width;
+                    coords.y = pCoords.y + parent.getWidth();
+                } else if (piece.corner == 'down') {
+                    coords.x = pCoords.x - piece.width;
+                    coords.y = pCoords.y;
+                } else {
+                    coords.x = pCoords.x - piece.width;
+                    coords.y = pCoords.y;
+                }
+                break;
+
+            case 'right':
+                if (piece.corner == 'up') {
+                    coords.x = pCoords.x + parent.getWidth();
+                    coords.y = pCoords.y + parent.getWidth();
+                } else if (piece.corner == 'down') {
+                    coords.x = pCoords.x + parent.getWidth();
+                    coords.y = pCoords.y;
+                } else {
+                    coords.x = pCoords.x + parent.getWidth();
+                    coords.y = pCoords.y;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        piece.setCoords(coords);
     }
 }
 
