0c42576082ae70e08acbccda7298354f1be6a7d8
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-26 13:03:43 +0200

    Remove controllers.

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -7,36 +7,5 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    /**
-     * Return a new response from the application.
-     *
-     * @param  string $content
-     * @param  int    $status
-     * @param  array  $headers
-     * @return \Symfony\Component\HttpFoundation\Response|\Laravel\Lumen\Http\ResponseFactory
-     */
-    public function response($content = '', $status = 200, array $headers = [])
-    {
-        $factory = new ResponseFactory;
 
-        if (func_num_args() === 0) {
-            return $factory;
-        }
-
-        return $factory->make($content, $status, $headers);
-    }
-
-    /**
-     * Return a new JSON response from the application.
-     *
-     * @param  string $content
-     * @param  int    $status
-     * @param  array  $headers
-     * @param  int    $options
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function json($content = '', $status = 200, array $headers = [], $options = 0)
-    {
-        return $this->response()->json($content, $status, $headers, $options);
-    }
 }
