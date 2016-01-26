e6a63dd144c81ec5651a0bc966fab5b1b1a62f8f
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-26 12:58:34 +0200

    Refactor sw file.

--- sw.js
+++ sw.js
@@ -1,54 +1,68 @@
-var CACHE_NAME = 'dominox.app.cache';
+(function(){
+    var CACHE_NAME = 'dominox.cache';
 
-var requests = [
-    '/dominox/',
-    '/dominox/dist/js/app.js',
-    '/dominox/dist/css/app.css',
-    '/dominox/dist/img/logo.png',
-    'https://fonts.gstatic.com/s/lato/v11/1YwB1sO8YE1Lyjf12WNiUA.woff2',
-    'https://fonts.gstatic.com/s/lato/v11/EsvMC5un3kjyUhB9ZEPPwg.woff2',
-    'https://fonts.googleapis.com/css?family=Lato:100,300,400,700'
-];
+    var requests = [
+        '',
+        '/dist/js/app.js',
+        '/dist/css/app.css',
+        '/dist/img/logo.png',
+        '/dist/img/bg.jpg',
+        '/dist/img/bones.jpg',
+        '/dist/img/bones2.jpg'
+    ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Cached requests added!');
-                return cache.addAll(requests);
-            })
-            .catch(function(err) {
-                console.log(err);
-            })
-    );
-});
+    var external = [
+        'https://fonts.gstatic.com/s/lato/v11/1YwB1sO8YE1Lyjf12WNiUA.woff2',
+        'https://fonts.gstatic.com/s/lato/v11/EsvMC5un3kjyUhB9ZEPPwg.woff2',
+        'https://fonts.googleapis.com/css?family=Lato:100,300,400,700'
+    ];
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                }
+    for (let i = 0; i < requests.length; i++) {
+        requests = window.location.pathname + requests[i];
+    }
 
-                var fetchRequest = event.request.clone();
+    requests = requests.concat(external);
 
-                return fetch(fetchRequest).then(
-                    function(response) {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+    self.addEventListener('install', function(e) {
+        e.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    console.log('Cached requests added!');
+                    return cache.addAll(requests);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                })
+        );
+    });
 
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
+    self.addEventListener('fetch', function(e) {
+        e.respondWith(
+            caches.match(e.request)
+                .then(function(response) {
+                    if (response) {
                         return response;
                     }
-                );
-            })
-    );
-});
+
+                    var fetchRequest = e.request.clone();
+
+                    return fetch(fetchRequest).then(
+                        function(response) {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+
+                            var responseToCache = response.clone();
+
+                            caches.open(CACHE_NAME)
+                                .then(function(cache) {
+                                    cache.put(e.request, responseToCache);
+                                });
+
+                            return response;
+                        }
+                    );
+                })
+        );
+    });
+})();
