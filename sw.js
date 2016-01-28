d16894e6f9bf650871b16acef2253582e57e6c44
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-28 07:46:27 +0200

    Fix service worker.

--- sw.js
+++ sw.js
@@ -1,68 +1,70 @@
-(function(){
-    var CACHE_NAME = 'dominox.cache';
+var CACHE_NAME = 'dominox.cache';
 
-    var requests = [
-        '',
-        '/dist/js/app.js',
-        '/dist/css/app.css',
-        '/dist/img/logo.png',
-        '/dist/img/bg.jpg',
-        '/dist/img/bones.jpg',
-        '/dist/img/bones2.jpg'
-    ];
+var pathname = '/dominox/';
 
-    var external = [
-        'https://fonts.gstatic.com/s/lato/v11/1YwB1sO8YE1Lyjf12WNiUA.woff2',
-        'https://fonts.gstatic.com/s/lato/v11/EsvMC5un3kjyUhB9ZEPPwg.woff2',
-        'https://fonts.googleapis.com/css?family=Lato:100,300,400,700'
-    ];
+var requests = [
+    '',
+    '/dist/js/app.js',
+    '/dist/css/app.css',
+    '/dist/img/logo.png',
+    '/dist/img/logo_test.png',
+    '/dist/img/bg.jpg',
+    '/dist/img/bones.jpg',
+    '/dist/img/bones2.jpg',
+    '/dist/img/game.jpg'
+];
 
-    for (let i = 0; i < requests.length; i++) {
-        requests = window.location.pathname + requests[i];
-    }
+var external = [
+    'https://fonts.gstatic.com/s/lato/v11/1YwB1sO8YE1Lyjf12WNiUA.woff2',
+    'https://fonts.gstatic.com/s/lato/v11/EsvMC5un3kjyUhB9ZEPPwg.woff2',
+    'https://fonts.googleapis.com/css?family=Lato:100,300,400,700'
+];
 
-    requests = requests.concat(external);
+for (var i = 0; i < requests.length; i++) {
+    requests = pathname + requests[i];
+}
 
-    self.addEventListener('install', function(e) {
-        e.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    console.log('Cached requests added!');
-                    return cache.addAll(requests);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                })
-        );
-    });
+requests = requests.concat(external);
 
-    self.addEventListener('fetch', function(e) {
-        e.respondWith(
-            caches.match(e.request)
-                .then(function(response) {
-                    if (response) {
-                        return response;
-                    }
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Cached requests added!');
+                return cache.addAll(requests);
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
+    );
+});
 
-                    var fetchRequest = e.request.clone();
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
 
-                    return fetch(fetchRequest).then(
-                        function(response) {
-                            if (!response || response.status !== 200 || response.type !== 'basic') {
-                                return response;
-                            }
+                var fetchRequest = e.request.clone();
 
-                            var responseToCache = response.clone();
-
-                            caches.open(CACHE_NAME)
-                                .then(function(cache) {
-                                    cache.put(e.request, responseToCache);
-                                });
-
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
-                    );
-                })
-        );
-    });
-})();
+
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(e.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
