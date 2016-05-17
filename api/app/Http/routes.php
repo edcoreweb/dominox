2e84a8e87bc1999f8cffb4df09a14f282d701322
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-05-17 08:52:47 +0300

    Set app url.

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -9,7 +9,7 @@ $app->get('notification', function () use ($app) {
     $payload = json_decode($notif->payload);
 
     return [
-        'url' => 'http://localhost/dominox/#!/join/' . $payload->hash,
+        'url' => env('APP_URL') . '/#!/join/' . $payload->hash,
         'title' => 'You have been invited to a game!',
         'body' => $payload->name,
     ];
